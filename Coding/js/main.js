71d94a82b2453410e605b7dc160d92aafb88fa7d
Nezzer <bnes93@gmail.com>
2026-07-26 22:12:31 -0400

    added another 7 Kyu challenge completed

--- Coding/js/main.js
+++ Coding/js/main.js
@@ -9,3 +9,16 @@ The input string will only consist of lower case letters and/or spaces.
 function getCount(str) {
     return str.split('').filter(vowels => "aeiouAEIOU".includes(vowels)).length;
 }
+/* Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+
+For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1. (81-1-1-81)
+
+Example #2: An input of 765 will/should return 493625 because 72 is 49, 62 is 36, and 52 is 25. (49-36-25)
+
+Note: The function accepts an integer and returns an integer. */
+
+
+function squareDigits(num){
+    let arr = num.toString().split('').map((e) => e**2).join('')
+    return Number(arr)
+  }
