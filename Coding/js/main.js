a1e20177e1d47301c69247f872b4bda48cd07377
Nezzer <bnes93@gmail.com>
2026-07-30 15:26:46 -0400

    Completed an 8 Kyu Kata Named String repeat

--- Coding/js/main.js
+++ Coding/js/main.js
@@ -22,3 +22,12 @@ function squareDigits(num){
     let arr = num.toString().split('').map((e) => e**2).join('')
     return Number(arr)
   }
+
+/* Write a function that accepts a non-negative integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+Examples (input -> output)
+6, "I"     -> "IIIIII"
+5, "Hello" -> "HelloHelloHelloHelloHello" */
+
+function repeatStr (n, s) {
+    return s.repeat(n)
+}
