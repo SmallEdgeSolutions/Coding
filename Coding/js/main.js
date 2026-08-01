70e09ac3b39cfdcdf5d81de73d1548fc0eabd9ff
Nezzer <bnes93@gmail.com>
2026-07-31 22:18:41 -0400

    added The JS Way OOP hw and 8kyu kata

--- Coding/js/main.js
+++ Coding/js/main.js
@@ -31,3 +31,90 @@ Examples (input -> output)
 function repeatStr (n, s) {
     return s.repeat(n)
 }
+
+
+/*
+
+    The jsWay OOP Chapter
+
+
+*/
+
+// const aurora = {
+//     name: "Aurora",
+//     health: 150,
+//     strength: 25,
+//     xp: 0,
+  
+//     // Return the character description
+//     describe() {
+//       return `${this.name} has ${this.health} health points, ${this
+//         .strength} as strength and ${this.xp} XP points`;
+//     }
+//   };
+  
+//   // Aurora is harmed by an arrow
+//   aurora.health -= 20;
+  
+//   // Aurora gains a strength necklace
+//   aurora.strength += 10;
+  
+//   // Aurora learns a new skill
+//   aurora.xp += 15;
+  
+//   console.log(aurora.describe());
+
+  // Creating a Character Class as a part of JSWay OOP --- The JS Way OOP will continue until posted otherwise
+
+  class Character {
+    constructor(name, health, strength){
+        this.name = name
+        this.health = health
+        this.strength = strength
+        this.xp = 0 // XP is always zero for new characters
+    } 
+    attack(target) {
+        if (this.health > 0) {
+            const damage = this.strength
+            console.log(
+                `${this.name} attacks ${target.name} and causes ${damage} damge points`
+            )
+            target.health -= damage
+            if (target.health > 0){
+                console.log(`${target.name} has ${target.health} health points left`)
+            } else {
+                target.health = 0
+                const bonusXP = 10
+                console.log(
+                    `${this.name} eliminated ${target.name} and wins ${bonusXP} experience points`
+                )
+                this.xp += bonusXP
+            }
+        } else {
+            console.log(`${this.name} can't attack (they've been eliminated)`)
+        }
+    }
+    // Return the character description
+    describe() {
+        return `${this.name} has ${this.health} health points, ${this.strength} as strength and ${this.xp} XP points`
+    }
+  }
+
+  const aurora = new Character("Aurora", 150, 25)
+  const glacius = new Character("Glacius", 130, 30)
+
+  console.log("Welcome to the adventure! Here are our heroes:")
+  console.log(aurora.describe())
+  console.log(glacius.describe())
+
+  const monster = new Character("Spike", 40, 20)
+  console.log("A wild monstser has appeared: it's named " + monster.name)
+
+  monster.attack(aurora)
+  monster.attack(glacius)
+  aurora.attack(monster)
+  glacius.attack(monster)
+
+  console.log(aurora.describe())
+  console.log(glacius.describe())
+
