8de6ee68d2064f0d5df656ac37d2abda1473285a
Nezzer <bnes93@gmail.com>
2026-08-02 20:12:14 -0400

    added an 8Kyu Kata Named Opposites Attract

--- Coding/js/main.js
+++ Coding/js/main.js
@@ -138,3 +138,23 @@ function repeatStr (n, s) {
   // actually submitted 
   
   const paperwork = (n, m) => n <= 0 || m < 0 ?  0 : n * m
+
+  /* 8kyu Opposites Attract
+  -----------------------------------------------------------------
+    Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each. If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
+
+    Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
+-----------------------------------------------------------------
+  */
+
+// submitted.. no help
+
+
+function lovefunc(flower1, flower2){
+    let totalFlowers = flower1 + flower2
+   if(totalFlowers % 2 == 0){
+     return false
+   }else{
+     return true
+   }
+}
